8bdaa194c6af0cdf38c52cb78e97aff4a5d374c9
Bruce Moxon <brucemoxon@microsoft.com>
2021-10-23 14:06:54 -0700

    azurerm 2.82.0, azuread 2.7.0

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.43"
+      version = "~> 2.82.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 1.2.1"
+      version = "~> 2.7.0"
     }
   }
 }
