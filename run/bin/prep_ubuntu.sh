bf44fbe951466d87b3ceefee10c2c4bc3c5732fe
Bruce Moxon <brucemoxon@microsoft.com>
2021-11-07 16:08:24 -0800

    reorder pip upgrade

--- run/bin/prep_ubuntu.sh
+++ run/bin/prep_ubuntu.sh
@@ -2,16 +2,22 @@
 
 #-- helper script for environment prep on Ubuntu 18.04, 20.04, 20.10 ++>
 
-sudo apt update
+sudo apt-get update
 
-#sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
-sudo apt install -y jq redis-tools
-sudo apt install -y python3 python3-pip python3-venv
-python3 -m pip install --upgrade pip
+sudo apt-get install -y jq redis-tools
+sudo apt-get install -y python3 python3-pip python3-venv
 
 mkdir -p ~/pythonenvs
 python3 -m venv ~/pythonenvs/azfsenv
 . ~/pythonenvs/azfsenv/bin/activate
+python3 -m pip install --upgrade pip
+sudo cat > ~/.profile << EOF
+
+# activate python env
+source ~/pythonenvs/azfsenv/bin/activate
+EOF
+
 pip3 install -r ~/azfinsim-run/run/src/requirements.txt
 
