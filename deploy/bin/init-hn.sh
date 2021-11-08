be6e7efdc773912aa884e6497db5113b82b4dec4
Bruce Moxon <brucemoxon@microsoft.com>
2021-11-08 09:48:50 -0800

    .bash_profile populate in init-hn.sh

--- deploy/bin/init-hn.sh
+++ deploy/bin/init-hn.sh
@@ -3,6 +3,15 @@
 # init-hn.sh
 # init the environment for the headnode
 
+# installing az cli
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# installing terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository \
+	"deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt install terraform
+
 echo "pulling azfinsim/run from github"
 
 mkdir -p azfinsim-run
@@ -21,3 +30,19 @@ popd
 
 echo "prepping the runvm (ubuntu)"
 ./azfinsim-run/run/bin/prep_ubuntu.sh
+
+if [ ! -f ~/.bash_profile ]; then
+  echo ".bash_profile exists; not overwriting"
+else
+  echo "Creating .bash_profile"
+  sudo cat > ~/.bash_profile << EOF
+# activate python env
+source ~/pythonenvs/azfsenv/bin/activate
+
+# az login
+. ./azfinsim.config
+az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
+EOF
+fi
+
+
